f70b960288c6241752555aa75d3e467d8e283f35
Daemyon <damioncms@gmail.com>
2019-03-13 23:03:59 -0500

    Add Splash screen with transition

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const thread = document.getElementById('thread')
 const messages = document.getElementById('messages')
 
+window.onload = () => {
+  setTimeout(()=> {
+    let e = document.querySelector('.splash');
+    e.classList.remove('splash');
+  },4000)
+}
+
 thread.addEventListener('click', () => {
    
    messages.style.display = "block";     
